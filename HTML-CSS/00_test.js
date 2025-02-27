6232fa41994083b5e94f5562ff5865a771c014a7
yongsuk204 <yongsuk204@gmail.com>
2025-02-27 18:05:23 +0900

    1111

---
+++ HTML-CSS/00_test.js
@@ -0,0 +1,7 @@
+const btnStart = document.querySelector("button");
+// const라는 변수의 이름은 btnStart이고 = 선언한것
+btnStart.onclick = function () {
+  alert("start");
+};
+// 변수 btnStart에 onclick이라는 동작이 일어나면 alert가 실행된다
+// 세미콜론 ; 종료된다는 의미이고 문법적으로 필수적으로 해야함
